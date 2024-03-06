0752b10809322ed7ecfb00bfb61da3fe03a3dc89
Nicolai <134018297+NicolaiTesla4@users.noreply.github.com>
2024-03-05 21:53:09 -0500

    Three

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -10,7 +10,6 @@ const App = () => {
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ]);
-  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     const storedContacts = localStorage.getItem('contacts');
@@ -25,10 +24,6 @@ const App = () => {
 
   const addContact = (newContact) => {
     setContacts(prevContacts => [...prevContacts, newContact]);
-  };
-
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value);
   };
 
   const deleteContact = (contactId) => {
